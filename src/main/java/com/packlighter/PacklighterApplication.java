26e8a18d6994819c64cf56de8e553932491515e4
brianlouiss <brianlouis88@gmail.com>
2019-06-01 11:41:21 -0700

    initial commit

---
+++ src/main/java/com/packlighter/PacklighterApplication.java
@@ -0,0 +1,13 @@
+package com.packlighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacklighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacklighterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packlighter/PacklighterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.packlighter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PacklighterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
